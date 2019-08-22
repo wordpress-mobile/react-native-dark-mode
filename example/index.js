60d320e6f2917d27b106d615d790c4fb8e60b24d
etoledom <etoledom@icloud.com>
2019-08-22 23:57:16 +0200

    Remove yarn workspace

--- example/index.js
+++
@@ -1,7 +0,0 @@
-import { AppRegistry, YellowBox } from 'react-native'
-import App from './App'
-import { name as appName } from './app.json'
-
-YellowBox.ignoreWarnings(['Remote debugger'])
-
-AppRegistry.registerComponent(appName, () => App)
